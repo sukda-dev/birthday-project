b4fc6fed6489578582fe2389ea891c631d1fd25c
Bank Outsourcify <bank@outsourcify.net>
2025-09-02 17:58:21 +0700

    setup github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://sukda-dev.github.io/birthday-project", // URL เว็บไซต์จริง
-  base: "/birthday-project/", // ใช้ชื่อ repo ของคุณ
+  site: "https://sukda-dev.github.io",
+  base: "/birthday-project/",
 });
