48453876122bcdaadb86bf94964f2407dee74222
Bank Outsourcify <bank@outsourcify.net>
2025-09-02 18:16:46 +0700

    upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://sukda-dev.github.io",
-  base: "birthday-project",
+  site: "https://sukda-dev.github.io/birthday-project/",
+  base: "/birthday-project/",
 });
