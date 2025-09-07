e60284f764c2c2f5c2e497d9d93abb2c65b94e9c
Bank Outsourcify <bank@outsourcify.net>
2025-09-07 18:14:06 +0700

    [fix] path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,3 +4,9 @@ export default defineConfig({
   site: "https://sukda-dev.github.io/",
   base: "birthday-project",
 });
+
+// local
+// export default defineConfig({
+//   site: "https://sukda-dev.github.io/",
+//   base: "/",
+// });
